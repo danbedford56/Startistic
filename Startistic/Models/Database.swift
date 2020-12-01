7eb50a88c692ac254c027d078c9cd628cf9e73c9
Dan <daniel.bedford56@gmail.com>
2020-12-01 14:52:06 +0000

    Added send data to firebase

---
+++ Startistic/Models/Database.swift
@@ -0,0 +1,42 @@
+//
+//  Database.swift
+//  Startistic
+//
+//  Created by Dan Bedford on 01/12/2020.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class Database {
+    
+    private static var db = Firestore.firestore()
+    
+    static func fetchUsers() -> Array<Any> {
+        var dataArray: Array<Any> = []
+        db.collection("users").addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+
+            dataArray = documents.map { queryDocumentSnapshot -> User in
+                let data = queryDocumentSnapshot.data()
+                let username = data["username"] as? String ?? ""
+
+                return User(id: .init(), username: username)
+            }
+        }
+        return dataArray
+    }
+    
+    static func create_user(_ user: [String : Any]) {
+        do {
+            let _ = try db.collection("users").addDocument(data: user)
+        } catch {
+          print(error)
+        }
+    }
+    
+    
+}
